ba1f8ef771c98ccd428160535f40d04e272249d5
tothall <100217523+tothall@users.noreply.github.com>
2024-09-13 14:32:59 -0300

    Update and rename classe_veiculo.py to classe_Produto.py

---
+++ classe_Produto.py
@@ -0,0 +1,14 @@
+class Produto:
+    def __init__(self, placa, tipo, marca, modelo, cor, ano_fabricacao, data_validade, lote, quantidade, preco, status):
+        self.codigo = codigo
+        self.tipo = tipo
+        self.marca = marca
+        self.modelo = modelo
+        self.ano_fabricacao = ano_fabricacao
+        self.data_validade = data_validade
+        self.lote = lote
+        self.quantidade = quantidade
+        self.preco = preco
+        self.status = status
+        
+    
